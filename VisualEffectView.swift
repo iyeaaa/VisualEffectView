8b2aa488c4da2013e99347dbc427186df9c06c49
yein Yi <iyeaaa@o.cnu.ac.kr>
2023-10-01 09:08:41 +0000

    Create VisualEffectView.swift

---
+++ VisualEffectView.swift
@@ -0,0 +1,13 @@
+// MARK: - VisualEffectView
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { 
+        UIVisualEffectView()
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { 
+        uiView.effect = effect
+    }
+}
